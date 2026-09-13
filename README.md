f0d242917d1ff0e06c0bc189a8744e8c86bfee8a
Sanket Arekar <sanketarekar09@gmail.com>
2026-09-13 00:41:38 -0400

    adding project files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-ReadMe
+How to Set up and Run
+
+
+Steps:
+1. Use the pip install -r requirements.txt to load all the dependencies.
+2. Open terminal in your project.
+3. run Python train.py
+
---
+++ train.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+#Import the California housing dataset and convert it to a pandas DataFrame
+housing = fetch_california_housing()
+housing_df = pd.DataFrame(data=housing.data, columns=housing.feature_names)
+housing_df['MedianHouseValue'] = housing.target
+
+# Split the dataset into training and testing sets
+x_train, x_test, y_train, y_test = train_test_split(housing_df.iloc[:, :-1], housing_df.iloc[:, -1], test_size=0.2, random_state=42)
+
+# Train a linear regression model on the training data
+model = LinearRegression()
+model.fit(x_train, y_train)
+
+# Make predictions on the test data
+y_pred = model.predict(x_test)
+
+# Evaluate the model's performance using the R-squared score
+model_score = model.score(x_test, y_test)
+
+
+if __name__ == "__main__":
+    # print(housing)
+    # print(housing.data.shape, housing.target.shape)
+    print(housing_df.head())
+    # print(x_train, y_train, x_test, y_test)
+    print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+    print("Model R-squared score:", model_score)
+
